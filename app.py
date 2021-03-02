e63cbcb06cd70c53948d53fdcd872bed87e467d8
[strong1133] <[strong1133@naver.com]>
2021-03-02 18:47:31 +0900

    [merge]

---
+++ app.py
@@ -0,0 +1,18 @@
+import requests
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
