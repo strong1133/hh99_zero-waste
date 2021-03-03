d37a3416eae99c23b35046a0de4757e970b4dcff
[strong1133] <[strong1133@naver.com]>
2021-03-03 15:50:22 +0900

    [register add]

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import requests
 from flask import Flask, render_template
-from bs4 import BeautifulSoup
 from pymongo import MongoClient
-from pymongo import MongoClient
+import jwt
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.dbhh99
 
 
+######### HTML RENDER ############
+##################################
 @app.route('/')
 def home():
     articles = list(db.db_zerowaste.find({}, {'_id': False}))
@@ -19,11 +19,13 @@ def home():
 def register():
     return render_template('register.html')
 
-#ㅁㄴㅇ
+
 @app.route('/login')
 def login():
     return render_template('login.html')
 
 
+######### 로그인 ############
+##################################
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
--- static/register.js
+++ static/register.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
 })
 
-function joinblankcheck() {
+function join_blank_check() {
     let name = $('#register-name').val()
     let id = $('#register-id').val()
     let pw = $('#register-pw').val()
@@ -21,6 +21,46 @@ function joinblankcheck() {
         alert('이메일을 입력하세요')
         return;
     }
-    alert('가입을 환영합니다. '+name+'님')
+    if (!$("#help-id").hasClass("is-checked")) {
+        alert("아이디 중복확인을 해주세요.")
+        return;
+    }
+    if (checkPassword(pw) == false) {
+        return;
+    }
+
+    alert('가입을 환영합니다. ' + name + '님')
     window.location.href = "/login"
 }
+
+function overlap_check() {
+    let id = $('#register-id').val()
+    if (id == '') {
+        alert('아이디를 입력하세요')
+        return;
+    }
+    $('#help-id').addClass('is-checked')
+    alert('사용가능한 아이디 입니다.')
+}
+
+function checkPassword(pw) {
+    if (!/^[a-zA-Z0-9]{5,15}$/.test(pw)) {
+        alert('비밀번호 오류!! 숫자와 영문자 조합으로 10~15자리를 사용해야 합니다.');
+        return false;
+    }
+    var checkNumber = pw.search(/[0-9]/g);
+    var checkEnglish = pw.search(/[a-z]/ig);
+
+    if (checkNumber < 0 || checkEnglish < 0) {
+        alert("비밀번호 오류!! 숫자와 영문자를 혼용하여야 합니다.");
+        return false;
+    }
+
+    if (/(\w)\1\1\1/.test(pw)) {
+        alert('비밀번호 오류!! 444같은 문자를 4번 이상 사용하실 수 없습니다.');
+        return false;
+    }
+    return true;
+}
+
+
